58f3d0c8b8ca6e0a2e2ad446f3b0230b34acab9c
froilanisidro2 <145437259+froilanisidro2@users.noreply.github.com>
2024-04-06 09:01:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const auth = (req,res,  next) => {
 	next();
 }
 app.get('/',  (req, res) => {
-	res.send('Hello Froilan');
+	res.send('Hello World!!!!!');
   })
 app.get('/testgetrequest',  (req, res) => {
 	res.send('Hello Froilan2');
