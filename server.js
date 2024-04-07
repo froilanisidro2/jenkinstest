3b55db4a8b5b9db17994c382d75c4f670afe23e1
froilanisidro2 <145437259+froilanisidro2@users.noreply.github.com>
2024-04-07 09:36:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -21,16 +21,7 @@ const auth = (req,res,  next) => {
 app.get('/',  (req, res) => {
 	res.send('Hello 3002!!!!!');
   })
-app.get('/testgetrequest',  (req, res) => {
-	res.send('Hello Froilan2');
-  })
-app.get('/testwithheader', auth,  (req, res) => {
-  res.send('Hello World3! with header');
-})
-app.post('/testwithheader', auth ,jsonParser,  (req, res) => {
-	//console.log(req.body);
-	res.send('Hello World4! with header n body'+req.body.name);
-})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
